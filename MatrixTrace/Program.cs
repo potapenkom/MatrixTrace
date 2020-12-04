9f6cd2587511c991bdddf128215431e2e0ad8ff1
Misha Potapenko <potapenko05121985@gmail.com>
2020-12-05 01:17:40 +0200

    first commit

---
+++ MatrixTrace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
